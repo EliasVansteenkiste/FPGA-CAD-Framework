e6f6dd2c3b177f846cc713a2fa997fc25a8fa70b
Arno Messiaen <arno.messiaen@ugent.be>
2015-02-23 12:44:04 +0100

    Fixed bugs an linear part analytical placer

--- src/placers/analyticalplacer/AnalyticalPlacer.java
+++ src/placers/analyticalplacer/AnalyticalPlacer.java
@@ -182,160 +182,341 @@ public class AnalyticalPlacer
 				}
 			}
 			
-			for(Integer index:netClbIndices)
+			
+			if(net.name.contains("pv22_5"))
 			{
-				double deltaMaxX = Math.abs(linearX[index] - maxX);
-				if(deltaMaxX == 0)
+				System.out.println("MAX_X: " + maxX + ",\tIndex: " + maxXIndex);
+				System.out.println("MIN_X: " + minX + ",\tIndex: " + minXIndex);
+				System.out.println("MAX_Y: " + maxY + ",\tIndex: " + maxYIndex);
+				System.out.println("MIN_Y: " + minY + ",\tIndex: " + minYIndex);
+			}
+			
+			
+			
+			
+			
+			//Add connection beween min and max
+			if(!(minXIndex == -1 && maxXIndex == -1))
+			{
+				double delta = maxX - minX;
+				if(delta == 0)
 				{
-					deltaMaxX = 0.001;
+					delta = 0.001;
 				}
-				double weightMaxX = (2/(nbPins-1)) * (1/deltaMaxX);
-				if(maxXIndex == -1) //maxX is a fixed block
+				double weight = ((double)2/(nbPins-1)) * (1/delta);
+				if(maxXIndex == -1)
 				{
-					//Connection between fixed and non fixed block
-					xMatrix.setElement(index, index, xMatrix.getElement(index, index) + weightMaxX);
-					xVector[index] = xVector[index] + weightMaxX*maxX;
+					//maxX fixed but minX not
+					xMatrix.setElement(minXIndex, minXIndex, xMatrix.getElement(minXIndex, minXIndex) + weight);
+					xVector[minXIndex] = xVector[minXIndex] + weight*maxX;
 				}
-				else //maxX is not a fixed block
+				else
 				{
-					//Connection between two non fixed blocks
-					if(!(maxXIndex == index))
+					if(minXIndex == -1)
 					{
-						xMatrix.setElement(index, index, xMatrix.getElement(index, index) + weightMaxX);
-						xMatrix.setElement(maxXIndex, maxXIndex, xMatrix.getElement(maxXIndex, maxXIndex) + weightMaxX);
-						xMatrix.setElement(index, maxXIndex, xMatrix.getElement(index, maxXIndex) - weightMaxX);
-						xMatrix.setElement(maxXIndex, index, xMatrix.getElement(maxXIndex, index) - weightMaxX);
+						//minX fixed but maxX not
+						xMatrix.setElement(maxXIndex, maxXIndex, xMatrix.getElement(maxXIndex, maxXIndex) + weight);
+						xVector[maxXIndex] = xVector[maxXIndex] + weight*minX;
 					}
-				}
-				
-				double deltaMinX = Math.abs(linearX[index] - minX);
-				if(deltaMinX == 0)
-				{
-					deltaMinX = 0.001;
-				}
-				double weightMinX = (2/(nbPins-1)) * (1/deltaMinX);
-				if(minXIndex == -1) //maxX is a fixed block
-				{
-					//Connection between fixed and non fixed block
-					xMatrix.setElement(index, index, xMatrix.getElement(index, index) + weightMinX);
-					xVector[index] = xVector[index] + weightMinX*minX;
-				}
-				else //maxX is not a fixed block
-				{
-					//Connection between two non fixed blocks
-					if(!(minXIndex == index))
+					else
 					{
-						xMatrix.setElement(index, index, xMatrix.getElement(index, index) + weightMinX);
-						xMatrix.setElement(minXIndex, minXIndex, xMatrix.getElement(minXIndex, minXIndex) + weightMinX);
-						xMatrix.setElement(index, minXIndex, xMatrix.getElement(index, minXIndex) - weightMinX);
-						xMatrix.setElement(minXIndex, index, xMatrix.getElement(minXIndex, index) - weightMinX);
-					}
-				}
-				
-				double deltaMaxY = Math.abs(linearY[index] - maxY);
-				if(deltaMaxY == 0)
-				{
-					deltaMaxY = 0.001;
-				}
-				double weightMaxY = (2/(nbPins-1)) * (1/deltaMaxY);
-				if(maxYIndex == -1) //maxX is a fixed block
-				{
-					//Connection between fixed and non fixed block
-					yMatrix.setElement(index, index, yMatrix.getElement(index, index) + weightMaxY);
-					yVector[index] = yVector[index] + weightMaxY*maxY;
-				}
-				else //maxX is not a fixed block
-				{
-					//Connection between two non fixed blocks
-					if(!(maxYIndex == index))
-					{
-						yMatrix.setElement(index, index, yMatrix.getElement(index, index) + weightMaxY);
-						yMatrix.setElement(maxYIndex, maxYIndex, yMatrix.getElement(maxYIndex, maxYIndex) + weightMaxY);
-						yMatrix.setElement(index, maxYIndex, yMatrix.getElement(index, maxYIndex) - weightMaxY);
-						yMatrix.setElement(maxYIndex, index, yMatrix.getElement(maxYIndex, index) - weightMaxY);
-					}
-				}
-				
-				double deltaMinY = Math.abs(linearY[index] - minY);
-				if(deltaMinY == 0)
-				{
-					deltaMinY = 0.001;
-				}
-				double weightMinY = (2/(nbPins-1)) * (1/deltaMinY);
-				if(minYIndex == -1) //maxX is a fixed block
-				{
-					//Connection between fixed and non fixed block
-					yMatrix.setElement(index, index, yMatrix.getElement(index, index) + weightMinY);
-					yVector[index] = yVector[index] + weightMinY*minY;
-				}
-				else //maxX is not a fixed block
-				{
-					//Connection between two non fixed blocks
-					if(!(minYIndex == index))
-					{
-						yMatrix.setElement(index, index, yMatrix.getElement(index, index) + weightMinY);
-						yMatrix.setElement(minYIndex, minYIndex, yMatrix.getElement(minYIndex, minYIndex) + weightMinY);
-						yMatrix.setElement(index, minYIndex, yMatrix.getElement(index, minYIndex) - weightMinY);
-						yMatrix.setElement(minYIndex, index, yMatrix.getElement(minYIndex, index) - weightMinY);
+						//neither of both fixed
+						xMatrix.setElement(minXIndex, minXIndex, xMatrix.getElement(minXIndex, minXIndex) + weight);
+						xMatrix.setElement(maxXIndex, maxXIndex, xMatrix.getElement(maxXIndex, maxXIndex) + weight);
+						xMatrix.setElement(minXIndex, maxXIndex, xMatrix.getElement(minXIndex, maxXIndex) - weight);
+						xMatrix.setElement(maxXIndex, minXIndex, xMatrix.getElement(maxXIndex, minXIndex) - weight);
 					}
 				}
 			}
 			
-			for(double fixedXPosition:fixedXPositions)
+			if(!(minYIndex == -1 && maxYIndex == -1))
 			{
-				if(!(maxXIndex == -1))
+				double delta = maxY - minY;
+				if(delta == 0)
 				{
-					double deltaMaxX = Math.abs(fixedXPosition - maxX);
+					delta = 0.001;
+				}
+				double weight = ((double)2/(nbPins-1)) * (1/delta);
+				if(maxYIndex == -1)
+				{
+					//maxX fixed but minX not
+					yMatrix.setElement(minYIndex, minYIndex, yMatrix.getElement(minYIndex, minYIndex) + weight);
+					yVector[minYIndex] = yVector[minYIndex] + weight*maxY;
+				}
+				else
+				{
+					if(minYIndex == -1)
+					{
+						//minX fixed but maxX not
+						yMatrix.setElement(maxYIndex, maxYIndex, yMatrix.getElement(maxYIndex, maxYIndex) + weight);
+						yVector[maxYIndex] = yVector[maxYIndex] + weight*minY;
+					}
+					else
+					{
+						//neither of both fixed
+						yMatrix.setElement(minYIndex, minYIndex, yMatrix.getElement(minYIndex, minYIndex) + weight);
+						yMatrix.setElement(maxYIndex, maxYIndex, yMatrix.getElement(maxYIndex, maxYIndex) + weight);
+						yMatrix.setElement(minYIndex, maxYIndex, yMatrix.getElement(minYIndex, maxYIndex) - weight);
+						yMatrix.setElement(maxYIndex, minYIndex, yMatrix.getElement(maxYIndex, minYIndex) - weight);
+					}
+				}
+			}
+			
+			//Add connections internal to max and min
+			for(Integer index:netClbIndices)
+			{
+				if(index != minXIndex)
+				{
+					double deltaMaxX = Math.abs(linearX[index] - maxX);
 					if(deltaMaxX == 0)
 					{
 						deltaMaxX = 0.001;
 					}
-					double weightMaxX = (2/(nbPins-1)) * (1/deltaMaxX);
-					//Connection between fixed and non fixed block
-					xMatrix.setElement(maxXIndex, maxXIndex, xMatrix.getElement(maxXIndex, maxXIndex) + weightMaxX);
-					xVector[maxXIndex] = xVector[maxXIndex] + weightMaxX*fixedXPosition;
+					double weightMaxX = ((double)2/(nbPins-1)) * (1/deltaMaxX);
+					if(maxXIndex == -1) //maxX is a fixed block
+					{
+						//Connection between fixed and non fixed block
+						xMatrix.setElement(index, index, xMatrix.getElement(index, index) + weightMaxX);
+						xVector[index] = xVector[index] + weightMaxX*maxX;
+						
+						
+						if(index == 0)
+						{
+							System.out.println("New (0,0) value: " + xMatrix.getElement(0, 0));
+							if(net.name.contains("pv22_5"))
+							{
+								System.out.println("HERE 1");
+							}
+						}
+						
+					}
+					else //maxX is not a fixed block
+					{
+						//Connection between two non fixed blocks
+						if(!(maxXIndex == index))
+						{
+							xMatrix.setElement(index, index, xMatrix.getElement(index, index) + weightMaxX);
+							xMatrix.setElement(maxXIndex, maxXIndex, xMatrix.getElement(maxXIndex, maxXIndex) + weightMaxX);
+							xMatrix.setElement(index, maxXIndex, xMatrix.getElement(index, maxXIndex) - weightMaxX);
+							xMatrix.setElement(maxXIndex, index, xMatrix.getElement(maxXIndex, index) - weightMaxX);
+							
+							if(index == 0 || maxXIndex == 0)
+							{
+								System.out.println("New (0,0) value: " + xMatrix.getElement(0, 0));
+								if(net.name.contains("pv22_5"))
+								{
+									System.out.println("HERE 2: weight = " + weightMaxX + ", delta = " + deltaMaxX);
+								}
+							}
+						}
+					}
 				}
 				
-				if(!(minXIndex == -1))
+				if(index != maxXIndex)
 				{
-					double deltaMinX = Math.abs(fixedXPosition - minX);
+					double deltaMinX = Math.abs(linearX[index] - minX);
 					if(deltaMinX == 0)
 					{
 						deltaMinX = 0.001;
 					}
-					double weightMinX = (2/(nbPins-1)) * (1/deltaMinX);
-					//Connection between fixed and non fixed block
-					xMatrix.setElement(minXIndex, minXIndex, xMatrix.getElement(minXIndex, minXIndex) + weightMinX);
-					xVector[minXIndex] = xVector[minXIndex] + weightMinX*fixedXPosition;
+					double weightMinX = ((double)2/(nbPins-1)) * (1/deltaMinX);
+					if(minXIndex == -1) //maxX is a fixed block
+					{
+						//Connection between fixed and non fixed block
+						xMatrix.setElement(index, index, xMatrix.getElement(index, index) + weightMinX);
+						xVector[index] = xVector[index] + weightMinX*minX;
+						
+						if(index == 0)
+						{
+							System.out.println("New (0,0) value: " + xMatrix.getElement(0, 0));
+							if(net.name.contains("pv22_5"))
+							{
+								System.out.println("HERE 3: weight = " + weightMinX + ", delta = " + deltaMinX);
+							}
+						}
+					}
+					else //maxX is not a fixed block
+					{
+						//Connection between two non fixed blocks
+						if(!(minXIndex == index))
+						{
+							xMatrix.setElement(index, index, xMatrix.getElement(index, index) + weightMinX);
+							xMatrix.setElement(minXIndex, minXIndex, xMatrix.getElement(minXIndex, minXIndex) + weightMinX);
+							xMatrix.setElement(index, minXIndex, xMatrix.getElement(index, minXIndex) - weightMinX);
+							xMatrix.setElement(minXIndex, index, xMatrix.getElement(minXIndex, index) - weightMinX);
+							
+							if(index == 0 || minXIndex == 0)
+							{
+								System.out.println("New (0,0) value: " + xMatrix.getElement(0, 0));
+								if(net.name.contains("pv22_5"))
+								{
+									System.out.println("HERE 4");
+								}
+							}
+						}
+					}
 				}
-			}
-			
-			for(double fixedYPosition:fixedYPositions)
-			{
-				if(!(maxYIndex == -1))
+				
+				if(index != minYIndex)
 				{
-					double deltaMaxY = Math.abs(fixedYPosition - maxY);
+					double deltaMaxY = Math.abs(linearY[index] - maxY);
 					if(deltaMaxY == 0)
 					{
 						deltaMaxY = 0.001;
 					}
-					double weightMaxY = (2/(nbPins-1)) * (1/deltaMaxY);
-					//Connection between fixed and non fixed block
-					yMatrix.setElement(maxYIndex, maxYIndex, yMatrix.getElement(maxYIndex, maxYIndex) + weightMaxY);
-					yVector[maxYIndex] = yVector[maxYIndex] + weightMaxY*fixedYPosition;
+					double weightMaxY = ((double)2/(nbPins-1)) * (1/deltaMaxY);
+					if(maxYIndex == -1) //maxX is a fixed block
+					{
+						//Connection between fixed and non fixed block
+						yMatrix.setElement(index, index, yMatrix.getElement(index, index) + weightMaxY);
+						yVector[index] = yVector[index] + weightMaxY*maxY;
+					}
+					else //maxX is not a fixed block
+					{
+						//Connection between two non fixed blocks
+						if(!(maxYIndex == index))
+						{
+							yMatrix.setElement(index, index, yMatrix.getElement(index, index) + weightMaxY);
+							yMatrix.setElement(maxYIndex, maxYIndex, yMatrix.getElement(maxYIndex, maxYIndex) + weightMaxY);
+							yMatrix.setElement(index, maxYIndex, yMatrix.getElement(index, maxYIndex) - weightMaxY);
+							yMatrix.setElement(maxYIndex, index, yMatrix.getElement(maxYIndex, index) - weightMaxY);
+						}
+					}
 				}
 				
-				if(!(minYIndex == -1))
+				if(index != maxYIndex)
 				{
-					double deltaMinY = Math.abs(fixedYPosition - minY);
+					double deltaMinY = Math.abs(linearY[index] - minY);
 					if(deltaMinY == 0)
 					{
 						deltaMinY = 0.001;
 					}
-					double weightMinY = (2/(nbPins-1)) * (1/deltaMinY);
-					//Connection between fixed and non fixed block
-					yMatrix.setElement(minYIndex, minYIndex, yMatrix.getElement(minYIndex, minYIndex) + weightMinY);
-					yVector[minYIndex] = yVector[minYIndex] + weightMinY*fixedYPosition;
+					double weightMinY = ((double)2/(nbPins-1)) * (1/deltaMinY);
+					if(minYIndex == -1) //maxX is a fixed block
+					{
+						//Connection between fixed and non fixed block
+						yMatrix.setElement(index, index, yMatrix.getElement(index, index) + weightMinY);
+						yVector[index] = yVector[index] + weightMinY*minY;
+					}
+					else //maxX is not a fixed block
+					{
+						//Connection between two non fixed blocks
+						if(!(minYIndex == index))
+						{
+							yMatrix.setElement(index, index, yMatrix.getElement(index, index) + weightMinY);
+							yMatrix.setElement(minYIndex, minYIndex, yMatrix.getElement(minYIndex, minYIndex) + weightMinY);
+							yMatrix.setElement(index, minYIndex, yMatrix.getElement(index, minYIndex) - weightMinY);
+							yMatrix.setElement(minYIndex, index, yMatrix.getElement(minYIndex, index) - weightMinY);
+						}
+					}
+				}
+			}
+			
+			boolean firstMax = true;
+			boolean firstMin = true;
+			for(double fixedXPosition:fixedXPositions)
+			{
+				if(!(fixedXPosition == minX && minXIndex == -1 && firstMax)) 
+				{
+					if(!(maxXIndex == -1))
+					{
+						double deltaMaxX = Math.abs(fixedXPosition - maxX);
+						if(deltaMaxX == 0)
+						{
+							deltaMaxX = 0.001;
+						}
+						double weightMaxX = ((double)2/(nbPins-1)) * (1/deltaMaxX);
+						//Connection between fixed and non fixed block
+						xMatrix.setElement(maxXIndex, maxXIndex, xMatrix.getElement(maxXIndex, maxXIndex) + weightMaxX);
+						xVector[maxXIndex] = xVector[maxXIndex] + weightMaxX*fixedXPosition;
+						
+						if(maxXIndex == 0)
+						{
+							System.out.println("New (0,0) value: " + xMatrix.getElement(0, 0));
+							if(net.name.contains("pv22_5"))
+							{
+								System.out.println("HERE 5");
+							}
+						}
+					}
+				}
+				else
+				{
+					firstMax = false;
+				}
+				
+				if(!(fixedXPosition == maxX && maxXIndex == -1 && firstMin))
+				{
+					if(!(minXIndex == -1))
+					{
+						double deltaMinX = Math.abs(fixedXPosition - minX);
+						if(deltaMinX == 0)
+						{
+							deltaMinX = 0.001;
+						}
+						double weightMinX = ((double)2/(nbPins-1)) * (1/deltaMinX);
+						//Connection between fixed and non fixed block
+						xMatrix.setElement(minXIndex, minXIndex, xMatrix.getElement(minXIndex, minXIndex) + weightMinX);
+						xVector[minXIndex] = xVector[minXIndex] + weightMinX*fixedXPosition;
+						
+						if(minXIndex == 0)
+						{
+							System.out.println("New (0,0) value: " + xMatrix.getElement(0, 0));
+							if(net.name.contains("pv22_5"))
+							{
+								System.out.println("HERE 6");
+							}
+						}
+					}
+				}
+				else
+				{
+					firstMin = false;
+				}
+			}
+			
+			firstMax = true;
+			firstMin = true;
+			for(double fixedYPosition:fixedYPositions)
+			{
+				if(!(fixedYPosition == minY && minYIndex == -1 && firstMax))
+				{
+					if(!(maxYIndex == -1))
+					{
+						double deltaMaxY = Math.abs(fixedYPosition - maxY);
+						if(deltaMaxY == 0)
+						{
+							deltaMaxY = 0.001;
+						}
+						double weightMaxY = ((double)2/(nbPins-1)) * (1/deltaMaxY);
+						//Connection between fixed and non fixed block
+						yMatrix.setElement(maxYIndex, maxYIndex, yMatrix.getElement(maxYIndex, maxYIndex) + weightMaxY);
+						yVector[maxYIndex] = yVector[maxYIndex] + weightMaxY*fixedYPosition;
+					}
+				}
+				else
+				{
+					firstMax = false;
+				}
+				
+				if(!(fixedYPosition == maxY && maxYIndex == -1 && firstMin))
+				{
+					if(!(minYIndex == -1))
+					{
+						double deltaMinY = Math.abs(fixedYPosition - minY);
+						if(deltaMinY == 0)
+						{
+							deltaMinY = 0.001;
+						}
+						double weightMinY = ((double)2/(nbPins-1)) * (1/deltaMinY);
+						//Connection between fixed and non fixed block
+						yMatrix.setElement(minYIndex, minYIndex, yMatrix.getElement(minYIndex, minYIndex) + weightMinY);
+						yVector[minYIndex] = yVector[minYIndex] + weightMinY*fixedYPosition;
+					}
+				}
+				else
+				{
+					firstMin = false;
 				}
 			}
 		}
@@ -396,6 +577,31 @@ public class AnalyticalPlacer
 		for(int i = 0; i < xSolution.length; i++)
 		{
 			System.out.println(xSolution[i] + "   ");
+		}
+		System.out.println("\n\n");
+		
+		System.out.println("YMatrix:");
+		for(int i = 0; i < yVector.length; i++)
+		{
+			for(int j = 0; j < yVector.length; j++)
+			{
+				System.out.print(yMatrix.getElement(i, j) + "       ");
+			}
+			System.out.println();
+		}
+		System.out.println("\n\n");
+		
+		System.out.println("YVector:");
+		for(int i = 0; i < yVector.length; i++)
+		{
+			System.out.println(yVector[i] + "   ");
+		}
+		System.out.println("\n\n");
+		
+		System.out.println("\n\nY-SOLUTION:");
+		for(int i = 0; i < ySolution.length; i++)
+		{
+			System.out.println(ySolution[i] + "   ");
 		}
 		System.out.println("\n\n");
 	}
