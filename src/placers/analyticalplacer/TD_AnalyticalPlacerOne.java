099b527c31283275faaaf5cd58bd669f4f404c63
Arno Messiaen <arno.messiaen@ugent.be>
2015-05-04 13:01:32 +0200

    Continued on TD analytical placement

--- src/placers/analyticalplacer/TD_AnalyticalPlacerOne.java
+++ src/placers/analyticalplacer/TD_AnalyticalPlacerOne.java
@@ -77,27 +77,14 @@ public class TD_AnalyticalPlacerOne
 			solveLinear(true, 0.0);
 		}
 		
-		double costLinear = calculateTotalCost(linearX, linearY);
-		System.out.println("Linear cost before legalizing = " + costLinear);
-		
 		//Initial legalization
 		legalizer.legalize(linearX, linearY, circuit.getNets().values(), indexMap);
 		
-		int nbIterations = 0;
 		for(int i = 0; i < 30; i++)
 		{
-			nbIterations++;
 			solveLinear(false, (i+1)*ALPHA);
 			legalizer.legalize(linearX, linearY, circuit.getNets().values(), indexMap);
-			costLinear = calculateTotalCost(linearX, linearY);
-			double costLegal = legalizer.calculateBestLegalCost(circuit.getNets().values(), indexMap);
-			if(costLinear / costLegal > 0.70)
-			{
-				break;
-			}
 		}
-		
-		System.out.println("Nb of iterations: " + nbIterations);
 		
 		updateCircuit();
 		
@@ -105,6 +92,11 @@ public class TD_AnalyticalPlacerOne
 		System.out.println("COST BEFORE REFINEMENT = " + cost);
 	}
 	
+	/*
+	 * If it are initial solves: 
+	 * 	- Don't add pseudoConnections
+	 *  - Don't add timing factor in weights
+	 */
 	public void solveLinear(boolean firstSolve, double pseudoWeightFactor)
 	{
 		int dimension = linearX.length;
@@ -144,32 +136,69 @@ public class TD_AnalyticalPlacerOne
 		for(Net net:circuit.getNets().values())
 		{
 			Block sourceOwner = net.source.owner;
-			int sourceX = sourceOwner.getSite().x;
-			int sourceY = sourceOwner.getSite().y;
+			int nbPins = 1 + net.sinks.size();
 			boolean isSourceFixed;
+			double sourceX;
+			double sourceY;
 			if(sourceOwner.type == BlockType.CLB)
 			{
 				isSourceFixed = false;
+				sourceX = linearX[indexMap.get(sourceOwner)];
+				sourceY = linearY[indexMap.get(sourceOwner)];
 			}
 			else
 			{
 				isSourceFixed = true;
+				sourceX = sourceOwner.getSite().x;
+				sourceY = sourceOwner.getSite().y;
 			}
 			for(Pin sinkPin: net.sinks)
 			{
 				Block sinkOwner = sinkPin.owner;
 				boolean isSinkFixed;
+				double sinkX;
+				double sinkY;
 				if(sinkOwner.type == BlockType.CLB)
 				{
 					isSinkFixed = false;
+					sinkX = linearX[indexMap.get(sinkOwner)];
+					sinkY = linearY[indexMap.get(sinkOwner)];
 				}
 				else
 				{
 					isSinkFixed = true;
+					sinkX = sinkOwner.getSite().x;
+					sinkY = sinkOwner.getSite().y;
 				}
 				if(!(isSourceFixed && isSinkFixed)) //Not both fixed
 				{
+					double weightX;
+					double weightY;
+					if(firstSolve) //Don't include timing factor
+					{
+						weightX = (double)2/((nbPins - 1)*Math.abs(sinkX - sourceX));
+						weightY = (double)2/((nbPins - 1)*Math.abs(sinkY - sourceY));
+					}
+					else //Include timing factor
+					{
+						jkgh
+					}
 					
+					if(isSourceFixed) //Source is fixed, sink is free
+					{
+						
+					}
+					else //Source is free
+					{
+						if(isSinkFixed) //Sink is fixed, source is free
+						{
+							
+						}
+						else //Both are free
+						{
+							
+						}
+					}
 				}
 			}
 		}
@@ -203,145 +232,6 @@ public class TD_AnalyticalPlacerOne
 			site.block = clb;
 			clb.setSite(site);
 		}
-	}
-	
-	Need to add criticalities to cost
-	private double calculateTotalCost(double[] xArray, double[] yArray)
-	{
-		double cost = 0.0;
-		for(Net net:circuit.nets.values())
-		{
-			double minX;
-			double maxX;
-			double minY;
-			double maxY;
-			Block sourceBlock = net.source.owner;
-			if(sourceBlock.type == BlockType.INPUT || sourceBlock.type == BlockType.OUTPUT)
-			{
-				minX = sourceBlock.getSite().x;
-				maxX = sourceBlock.getSite().x;
-				minY = sourceBlock.getSite().y;
-				maxY = sourceBlock.getSite().y;
-			}
-			else
-			{
-				int index = indexMap.get((Clb)sourceBlock);
-				minX = xArray[index];
-				maxX = xArray[index];
-				minY = yArray[index];
-				maxY = yArray[index];
-			}
-			
-			for(Pin pin:net.sinks)
-			{
-				Block sinkOwner = pin.owner;
-				if(sinkOwner.type == BlockType.INPUT || sinkOwner.type == BlockType.OUTPUT)
-				{
-					Site sinkOwnerSite = sinkOwner.getSite();
-					if(sinkOwnerSite.x < minX)
-					{
-						minX = sinkOwnerSite.x;
-					}
-					if(sinkOwnerSite.x > maxX)
-					{
-						maxX = sinkOwnerSite.x;
-					}
-					if(sinkOwnerSite.y < minY)
-					{
-						minY = sinkOwnerSite.y;
-					}
-					if(sinkOwnerSite.y > maxY)
-					{
-						maxY = sinkOwnerSite.y;
-					}
-				}
-				else
-				{
-					int index = indexMap.get((Clb)sinkOwner);
-					if(xArray[index] < minX)
-					{
-						minX = xArray[index];
-					}
-					if(xArray[index] > maxX)
-					{
-						maxX = xArray[index];
-					}
-					if(yArray[index] < minY)
-					{
-						minY = yArray[index];
-					}
-					if(yArray[index] > maxY)
-					{
-						maxY = yArray[index];
-					}
-				}
-			}
-			Set<Block> blocks = new HashSet<>();
-			blocks.addAll(net.blocks());
-			double weight = getWeight(blocks.size());
-			cost += ((maxX - minX) + (maxY - minY) + 2) * weight;
-			
-		}
-		return cost;
-	}
-	
-	private double getWeight(int size)
-	{
-		double weight = 0.0;
-		switch (size) {
-			case 1:  weight=1; break;
-			case 2:  weight=1; break;
-			case 3:  weight=1; break;
-			case 4:  weight=1.0828; break;
-			case 5:  weight=1.1536; break;
-			case 6:  weight=1.2206; break;
-			case 7:  weight=1.2823; break;
-			case 8:  weight=1.3385; break;
-			case 9:  weight=1.3991; break;
-			case 10: weight=1.4493; break;
-			case 11:
-			case 12:
-			case 13:
-			case 14:
-			case 15: weight=(size-10)*(1.6899-1.4493)/5+1.4493;break;				
-			case 16:
-			case 17:
-			case 18:
-			case 19:
-			case 20: weight=(size-15)*(1.8924-1.6899)/5+1.6899;break;
-			case 21:
-			case 22:
-			case 23:
-			case 24:
-			case 25: weight=(size-20)*(2.0743-1.8924)/5+1.8924;break;		
-			case 26:
-			case 27:
-			case 28:
-			case 29:
-			case 30: weight=(size-25)*(2.2334-2.0743)/5+2.0743;break;		
-			case 31:
-			case 32:
-			case 33:
-			case 34:
-			case 35: weight=(size-30)*(2.3895-2.2334)/5+2.2334;break;		
-			case 36:
-			case 37:
-			case 38:
-			case 39:
-			case 40: weight=(size-35)*(2.5356-2.3895)/5+2.3895;break;		
-			case 41:
-			case 42:
-			case 43:
-			case 44:
-			case 45: weight=(size-40)*(2.6625-2.5356)/5+2.5356;break;		
-			case 46:
-			case 47:
-			case 48:
-			case 49:
-			case 50: weight=(size-45)*(2.7933-2.6625)/5+2.6625;break;
-			default: weight=(size-50)*0.02616+2.7933;break;
-		}
-		return weight;
 	}
 	
 	private void initializeDataStructures()
