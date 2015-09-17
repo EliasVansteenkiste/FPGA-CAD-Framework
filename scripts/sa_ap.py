98dd234a815a5847881c5639828d3629a31f5ff7
Lebuin <seppe.lenders@gmail.com>
2015-09-17 13:01:38 +0200

    merge stash

--- scripts/sa_ap.py
+++ scripts/sa_ap.py
@@ -6,41 +6,34 @@ import re
 import csv
 
 #circuits = 'LU32PEEng LU8PEEng bgm blob_merge boundtop ch_intrinsics diffeq1 diffeq2 mcml mkDelayWorker32B mkPktMerge mkSMAdapter4B or1200 raygentop sha stereovision0 stereovision1 stereovision2 stereovision3'
-#circuits = 'blob_merge boundtop ch_intrinsics diffeq1 diffeq2 mkDelayWorker32B mkPktMerge mkSMAdapter4B or1200 raygentop sha stereovision0 stereovision1 stereovision2 stereovision3'
-circuits = 'alu4 apex2 apex4 bigkey clma des diffeq dsip elliptic ex1010 ex5p frisc misex3 pdc s298 s38417 s38584.1 seq spla tseng'
+circuits = 'blob_merge boundtop ch_intrinsics diffeq1 diffeq2 mkDelayWorker32B mkPktMerge mkSMAdapter4B or1200 raygentop sha stereovision0 stereovision1 stereovision2 stereovision3'
+
 
 circuit_list = circuits.split(' ')
-
-output_path = sys.argv[1]
-_file = open(output_path, 'w')
+_file = open('sa_ap.csv', 'w')
 csv_file = csv.writer(_file)
-csv_file.writerow(['benchmark', 'SA place time', 'SA WL cost', 'MDP place time', 'MDP WL cost'])
+csv_file.writerow(['benchmark', 'cost after SA', 'cost after AP'])
 
 os.chdir('..')
 
 
 for circuit in circuit_list:
     options = {
-        '--input': 'benchmarks/Blif',
+        '--input': '../../benchmarks/k6_N1_90nm_heterogeneous',
         '--circuit': circuit,
         '--architecture': '4lut',
-        '--placer': 'SA;MDP',
+        '--placer': 'SA;AP',
+        '--pack': '',
+        '--random': '',
     }
 
-    flags = [
-        '--pack',
-        '--random',
-<<<<<<< Updated upstream
-        'inner_num=0.1'
-=======
-        'inner_num=1'
->>>>>>> Stashed changes
-    ]
-
+    placer_options = {
+        'starting_stage': '0;1',
+    }
 
     # Call the java placer
     print('Placing {0}'.format(circuit))
-    out, err = call.placer(options, flags)
+    out, err = call.placer(options, placer_options)
 
     # If there is an error: print it and exit
     if err:
@@ -48,9 +41,11 @@ for circuit in circuit_list:
         print(err)
         sys.exit(1)
 
-    SA_time, SA_cost = call.get_stats(out, "SA")
-    MDP_time, MDP_cost = call.get_stats(out, "MDP")
+    regex = re.compile(r'SA\s+total cost:\s+(?P<SA>[0-9.]+).*AP\s+total cost:\s+(?P<AP>[0-9.]+)', re.DOTALL)
+    match = regex.search(out)
 
-    row = [circuit, SA_time, SA_cost, MDP_time, MDP_cost]
+    SA = match.group('SA')
+    AP = match.group('AP')
 
-    csv_file.writerow(row)
+    csv_file.writerow([circuit, SA, AP])
+    print([circuit, SA, AP])
