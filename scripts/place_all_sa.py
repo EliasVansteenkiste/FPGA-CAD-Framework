acfb9e7f2b9033a266d79d5c287930cd33e30abf
Lebuin <seppe.lenders@gmail.com>
2015-10-27 14:14:05 +0100

    add scripts

---
+++ scripts/place_all_sa.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os
+from place_and_route import PlaceCaller
+
+base_folder = 'benchmarks/'
+architecture = '10fle'
+placer = 'td_sa'
+options = ['timing_tradeoff=0.5']
+
+os.chdir('..')
+place_caller = PlaceCaller(base_folder)
+
+
+
+place_caller.place_all(architecture, placer)
---
+++ scripts/place_all_vpr.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+from place_and_route import VPRPlaceCaller
+
+base_folder = 'benchmarks/'
+input_folder = '10fle/'
+architecture = 'k6_frac_N10_mem32K_40nm'
+
+os.chdir('..')
+place_caller = VPRPlaceCaller(base_folder)
+
+
+output_folder = '10fle_place-wld_vpr/'
+place_caller.place_all(architecture, input_folder, output_folder, ['--timing_tradeoff', '0'])
+
+
+output_folder = '10fle_place-td_vpr/'
+place_caller.place_all(architecture, input_folder, output_folder, ['--timing_tradeoff', '0.5'])
