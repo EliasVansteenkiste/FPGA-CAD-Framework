d1bdab77753e51abc118e52eb245a43f8d8f1c36
Lebuin <seppe.lenders@gmail.com>
2015-11-23 14:37:00 +0100

    place script implemented

---
+++ scripts/place_all.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from place_and_route import PlaceCaller
+import os
+
+###############
+# Set options #
+###############
+
+architecture = 'benchmarks/k6_frac_N10_mem32K_40nm.xml'
+circuits_folder = 'benchmarks/'
+#circuits = 'bgm blob_merge boundtop ch_intrinsics diffeq1 diffeq2 LU32PEEng LU8PEEng mcml mkDelayWorker32B mkPktMerge mkSMAdapter4B or1200 raygentop sha stereovision0 stereovision1 stereovision2 stereovision3'
+circuits = 'boundtop ch_intrinsics'
+options = ['--placer', 'wld_gp']
+results_file = 'test'
+
+#####################
+# Place all ciruits #
+#####################
+os.chdir('..')
+place_caller = PlaceCaller(architecture, circuits_folder, circuits)
+place_caller.place_all(options, results_file)
--- scripts/place_and_route.py
+++ scripts/place_and_route.py
@@ -1,10 +1,16 @@
 import subprocess
-import os
+
+import errno
+import numpy
+import copy
+
 import re
 import csv
-import glob
+
 import sys
-import errno
+import os
+import glob
+import shutil
 
 
 def silentremove(filename):
@@ -15,25 +21,18 @@ def silentremove(filename):
             raise # re-raise exception if a different error occured
 
 
-class NewCaller:
+class Caller:
 
-    stats_metrics = ['Runtime', 'BB cost', 'Max delay']
-    stats_pattern = r'.*time\s+|\s+(?P<runtime>[0-9.e+-]+).*BB cost\s+|\s+(?P<bb_cost>[0-9.e+-]+).*max delay\s+|\s+(?P<max_delay>[0-9.e+-]+)'
-
-    def __init(self, base_folder, circuits=None):
-        self.base_folder = base_folder
-        if not self.base_folder.endswith('/'):
-            self.base_folder ++ '/'
-
-        if len(circuits) is None:
-            this.circuits = circuits
-
-        else:
-            this.circuits = glob.glob(base_folder + '*.net')
+    def __init__(self, circuits):
+        self.circuits = circuits.split(' ')
 
 
-    def call(self, command):
-        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    def call_circuit(self, command, circuit):
+        circuit_command = copy.deepcopy(command)
+        for i in range(len(circuit_command)):
+            circuit_command[i] = circuit_command[i].replace('{circuit}', circuit)
+
+        p = subprocess.Popen(circuit_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         # Return output
         out, err = p.communicate()
@@ -44,16 +43,14 @@ class NewCaller:
 
         stats_pattern = re.compile(self.stats_regex, re.DOTALL)
         self.results = {}
+        self.command = command
 
-        for circuit in this.circuits:
-            circuit_command = command.format(circuit)
-
-            out, err = self.call(command)
-            self.post_process_circuit(circuit)
+        for circuit in self.circuits:
+            out, err = self.call_circuit(command, circuit)
 
             if err:
-                print('There was a problem with circuit "{0}":'.format(circuit))
                 print(err)
+                print('There was a problem with circuit "{0}"'.format(circuit))
                 sys.exit(1)
 
             # Get and print statistics
@@ -61,7 +58,7 @@ class NewCaller:
 
             if match is None:
                 print(out)
-                print("Failed to match pattern: " + self.stats_regex)
+                print('Failed to match pattern: {0}'.format(self.stats_regex))
                 sys.exit(1)
 
             result = {}
@@ -72,10 +69,23 @@ class NewCaller:
             results[circuit] = result
 
 
-    def save_results(self, filename):
+    def save_results(self, filename, append=False):
+        if append:
+            mode = 'a'
+        else:
+            mode = 'w'
+
         _file = open(output_file, 'w')
         csv_writer = csv.writer(_file)
 
+        # Print the header
+        num_metrics = len(self.metrics)
+        row = [''] * (1 + num_metrics)
+        row.append(' '.join(self.command))
+        csv_file.writerow(row)
+        csv_file.writerow(['benchmark'] + self.metrics)
+
+        # Print the results for each
         for circuit in sorted(self.results):
             result = self.results[circuit]
 
@@ -87,6 +97,7 @@ class NewCaller:
             csv_writer.writerow(row)
             _file.flush()
 
+        csv_writer.writerow([])
         _file.close()
 
 
@@ -94,6 +105,8 @@ class NewCaller:
         geomeans = []
         for metric in self.metrics:
             geomeans.append(self.get_geomean(metric))
+
+        return geomeans
 
     def get_geomean(self, metric, circuits=None):
         if circuits is None:
@@ -103,292 +116,35 @@ class NewCaller:
         for circuit in circuits:
             metric_results.append(self.results[circuit][metric])
 
-        return math.geomean(metric_results)
-
-
-
-
-
-class Caller:
-    circuits = 'bgm blob_merge boundtop ch_intrinsics diffeq1 diffeq2 LU32PEEng LU8PEEng mcml mkDelayWorker32B mkPktMerge mkSMAdapter4B or1200 raygentop sha stereovision0 stereovision1 stereovision2 stereovision3'
-
-    def __init__(self, base_folder):
-        self.circuit_list = self.circuits.split(' ')
-
-        self.base_folder = base_folder
-        if self.base_folder[-1] != '/':
-            self.base_folder += '/'
-
-
-    def get_folder(self, architecture, placers=None):
-        if placers is None:
-            place_string = ''
-
-        else:
-            place_string = '_place-'
-
-            if isinstance(placers, str):
-                place_string += placers
-            else:
-                place_string += '+'.join(placers)
-
-
-        return self.base_folder + architecture + place_string + '/'
-
-
-    def print_message(self, message, input_placers):
-        if isinstance(input_placers, str):
-            placers = [input_placers]
-        else:
-            placers = input_placers
-
-        print(message + '+'.join(placers))
-
-
-    def call(self, command):
-        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        # Return output
-        out, err = p.communicate()
-        return out.decode('utf-8'), err.decode('utf-8')
-
-
-    def call_all(self, options, output_file):
-
-        stats_pattern = re.compile(self.stats_regex, re.DOTALL)
-
-        # Open the output csv file and write the header
-        output_folder = os.path.dirname(output_file)
-        if not os.path.exists(output_folder):
-            os.mkdir(output_folder)
-
-        _file = open(output_file, 'w')
-        csv_file = csv.writer(_file)
-
-        num_columns = len(self.stats_columns)
-        options_string = ' '.join(options)
-        csv_file.writerow([''] * (1 + num_columns) + [options_string])
-        csv_file.writerow(['benchmark'] + self.stats_columns)
-
-        # Loop through all circuits
-        for circuit in self.circuit_list:
-            # Call the java placer
-            print('  ' + circuit)
-            base_command = self.get_base_command()
-            circuit_options = self.get_circuit_options(circuit)
-
-            command = base_command + options + circuit_options
-            out, err = self.call(command)
-
-            # If there is an error: print it and exit
-            if err:
-                print('There was a problem with circuit "{0}":'.format(circuit))
-                print(err)
-                sys.exit(1)
-
-
-            # Get and print statistics
-            match = stats_pattern.search(out)
-
-            if match is None:
-                print(out)
-                print("Failed to match pattern: " + self.stats_regex)
-                sys.exit(1)
-
-            row = [circuit]
-            for column in self.stats_columns:
-                group_name = column.lower().replace(' ', '_')
-                row.append(match.group(group_name))
-
-            csv_file.writerow(row)
-            _file.flush()
-
-            self.post_process_circuit(circuit)
-
-        _file.close()
-
-
-
-class StatisticsCaller(Caller):
-
-    stats_columns = ['time', 'BB cost', 'max delay']
-    stats_regex = r'.*time:\s+(?P<time>[0-9.e+-]+).*\s+BB cost:\s+(?P<bb_cost>[0-9.e+-]+).*\s+max delay:\s+(?P<max_delay>[0-9.e+-]+)'
-
-    def place_all(self, architecture, input_folder, output_folder):
-        print("Printing statistics")
-
-        self.input_folder = self.base_folder + input_folder
-        self.output_folder = self.base_folder + output_folder
-
-        options = [
-            '--architecture', 'architectures/' + architecture + '.json',
-            '--input', self.output_folder,
-            '--output', self.output_folder
-        ]
-
-        output_file = self.output_folder + 'results_place.csv'
-
-        self.call_all(options, output_file)
-
-
-    def get_base_command(self):
-        return [
-            'java',
-            '-cp',
-            'bin:dependencies/args4j-2.32.jar:dependencies/json-simple-1.1.1.jar',
-            'interfaces.CLI'
-        ]
-
-
-    def get_circuit_options(self, circuit):
-        return ['--net_file', self.input_folder + circuit + '.net']
-
-    def post_process_circuit(self, circuit):
-        pass
-
-
-class VPRPlaceCaller(Caller):
-
-    stats_columns = ['time', 'BB cost', 'max delay']
-    stats_regex = r'(Placement estimated critical path delay: (?P<max_delay>[0-9.e+-]+) ns)?.*bb_cost: (?P<bb_cost>[0-9.e+-]+),.*Placement took (?P<time>[0-9.e+-]+) seconds'
-
-    def place_all(self, architecture, input_folder, output_folder, options=[]):
-        print("Placing with vpr to " + output_folder)
-
-        self.input_folder = self.base_folder + input_folder
-        self.output_folder = self.base_folder + output_folder
-        self.options = options
-
-        architecture_file = self.input_folder + architecture + '.xml'
-        output_file = self.output_folder + 'results_place_vpr.csv'
-
-        self.call_all([architecture_file], output_file)
-
-
-        # Print statistics
-        statistics_caller = StatisticsCaller(self.base_folder)
-        architecture = '10fle'
-        statistics_caller.place_all(architecture, input_folder, output_folder)
-
-
-    def get_base_command(self):
-        return ['../../vtr/vpr/vpr']
-
-
-    def get_circuit_options(self, circuit):
-        return [
-            circuit,
-            '--place',
-            '--blif_file', self.input_folder + circuit + '.blif',
-            '--net_file', self.input_folder + circuit + '.net',
-            '--place_file', self.output_folder + circuit + '.place',
-        ] + self.options
-
-
-    def post_process_circuit(self, circuit):
-        silentremove('vpr_stdout.log')
-
+        return numpy.gmean(metric_results)
 
 
 
 class PlaceCaller(Caller):
 
-    stats_columns = ['time', 'BB cost', 'max delay']
-    stats_regex = r'.*time:\s+(?P<time>[0-9.e+-]+).*\s+BB cost:\s+(?P<bb_cost>[0-9.e+-]+).*\s+max delay:\s+(?P<max_delay>[0-9.e+-]+)'
+    stats_metrics = ['Runtime', 'BB cost', 'Max delay']
+    stats_regex = r'.*time\s+|\s+(?P<runtime>[0-9.e+-]+).*BB cost\s+|\s+(?P<bb_cost>[0-9.e+-]+).*max delay\s+|\s+(?P<max_delay>[0-9.e+-]+)'
 
+    def __init__(self, architecture, circuits_folder, circuits):
+        super().__init__(circuits)
 
-    def place_all(self, architecture, placers, input_options=None):
-        self.print_message('Placing with ', placers)
+        self.architecture = architecture
+        self.circuit = os.path.join(circuits_folder, '{circuit}.blif')
 
-        # Set easy stuff
-        self.net_folder = self.get_folder(architecture)
-        output_folder = self.get_folder(architecture, placers)
-        architecture_file = 'architectures/' + architecture + '.json'
-        options = []
+    def place_all(self, options, results_file):
 
-        # Set the optional input placer file, and the current placer
-        if isinstance(placers, str):
-            placer = placers
-        else:
-            placer = placers[1]
-            input_place_folder = self.get_folder(architecture, placers[0])
-            options += ['--input', input_place_folder]
-
-        options += [
-            '--architecture', architecture_file,
-            '--output', output_folder,
-            '--placer', placer
-        ]
-        if input_options is not None:
-            options += input_options
-
-        # Run the caller
-        output_file = output_folder + 'results_place.csv'
-        self.call_all(options, output_file)
-
-
-    def get_base_command(self):
-        return [
+        command = [
             'java',
-            '-cp',
-            'bin:dependencies/args4j-2.32.jar:dependencies/json-simple-1.1.1.jar',
-            'interfaces.CLI'
+            '-cp', 'bin',
+            'interfaces.CLI',
+            self.architecture,
+            self.circuit,
+            '--output_place_file', 'tmp/{circuit}.place'
         ]
 
+        command += options
 
-    def get_circuit_options(self, circuit):
-        return ['--net_file', self.net_folder + circuit + '.net']
+        self.call_all_circuits(command)
+        self.save_results(results_file)
 
-    def post_process_circuit(self, circuit):
-        pass
-
-
-
-
-class RouteCaller(Caller):
-
-    stats_columns = ['time', 'BB cost', 'max delay']
-    stats_regex = r'Total wirelength: (?P<bb_cost>[0-9.e+-]+),.*Final critical path: (?P<max_delay>[0-9.e+-]+) ns.*Routing took (?P<time>[0-9.e+-]+) seconds'
-
-
-    def route_all(self, architecture, placers):
-        self.print_message('Routing from ', placers)
-
-        # Set folders
-        self.net_folder = self.get_folder(architecture)
-        self.place_folder = self.get_folder(architecture, placers)
-        self.route_folder = self.place_folder
-
-        # Get the architecture file
-        arch_files = glob.glob(self.net_folder + '/*.xml')
-        if len(arch_files) != 1:
-            print('Less or more than 1 architecture file found in input folder')
-            sys.exit(1)
-        arch_file = arch_files[0]
-
-        options = [arch_file]
-
-        # Run the caller
-        output_file = self.route_folder + 'results_route.csv'
-        self.call_all(options, output_file)
-
-
-    def get_base_command(self):
-        return ['../../vtr/vpr/vpr']
-
-
-    def get_circuit_options(self, circuit):
-        return [
-            circuit,
-            '--route',
-            '--blif_file', self.net_folder + circuit + '.blif',
-            '--net_file', self.net_folder + circuit + '.net',
-            '--place_file', self.place_folder + circuit + '.place',
-            '--route_file', self.route_folder + circuit + '.route',
-            '--route_chan_width', '300'
-        ]
-
-    def post_process_circuit(self, circuit):
-        os.remove(circuit + '.critical_path.out')
-        os.remove(circuit + '.slack.out')
-        os.remove('vpr_stdout.log')
+        shutil.rmtree('tmp')
