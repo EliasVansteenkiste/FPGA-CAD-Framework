ac9ec31fb0bd04645866d839eb7ba7de1c6f5b80
Lebuin <seppe.lenders@gmail.com>
2015-11-23 22:39:05 +0100

    change vars

--- scripts/parameter_sweep.py
+++ scripts/parameter_sweep.py
@@ -9,15 +9,11 @@ import os
 
 architecture = 'benchmarks/k6_frac_N10_mem32K_40nm.xml'
 circuits_folder = 'benchmarks/'
-circuits = 'stereovision3 ch_intrinsics'
+circuits = 'bgm blob_merge boundtop ch_intrinsics diffeq1 diffeq2 LU32PEEng LU8PEEng mcml mkDelayWorker32B mkPktMerge mkSMAdapter4B or1200 raygentop sha stereovision0 stereovision1 stereovision2 stereovision3'
 fixed_options = ['--placer', 'wld_ap']
 variable_options = {
     '--anchor_weight': [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6],
     '--anchor_weight_multiplier': [1.05, 1.1, 1.15, 1.2, 1.3],
-}
-variable_options = {
-    '--anchor_weight': [0.1, 0.3],
-    '--anchor_weight_multiplier': [1.05, 1.1],
 }
 
 ##########################################
--- scripts/place_all.py
+++ scripts/place_all.py
@@ -9,7 +9,7 @@ import os
 
 architecture = 'benchmarks/k6_frac_N10_mem32K_40nm.xml'
 circuits_folder = 'benchmarks/'
-#circuits = 'bgm blob_merge boundtop ch_intrinsics diffeq1 diffeq2 LU32PEEng LU8PEEng mcml mkDelayWorker32B mkPktMerge mkSMAdapter4B or1200 raygentop sha stereovision0 stereovision1 stereovision2 stereovision3'
+circuits = 'bgm blob_merge boundtop ch_intrinsics diffeq1 diffeq2 LU32PEEng LU8PEEng mcml mkDelayWorker32B mkPktMerge mkSMAdapter4B or1200 raygentop sha stereovision0 stereovision1 stereovision2 stereovision3'
 circuits = 'stereovision3 ch_intrinsics'
 options = ['--placer', 'wld_gp']
 results_file = 'test.csv'
