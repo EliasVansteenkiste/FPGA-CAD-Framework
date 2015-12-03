ee33848ac286d11b71c5247242d92c4c331d3e74
Lebuin <seppe.lenders@gmail.com>
2015-12-03 03:18:32 +0100

    multiple options sets in place_all

--- scripts/place_all.py
+++ scripts/place_all.py
@@ -12,15 +12,23 @@ architecture = 'benchmarks/k6_frac_N10_mem32K_40nm.xml'
 circuits_folder = 'benchmarks/'
 #circuits = 'bgm blob_merge boundtop ch_intrinsics diffeq1 diffeq2 LU32PEEng LU8PEEng mcml mkDelayWorker32B mkPktMerge mkSMAdapter4B or1200 raygentop sha stereovision0 stereovision1 stereovision2 stereovision3'
 circuits = 'bgm mcml LU32PEEng LU8PEEng stereovision2'
-options = ['--placer', 'td_gp', '--criticality_exponent', '4']
-results_file = 'place.csv'
-num_random_seeds = 4
+circuits = 'stereovision3'
+options = [
+    ['--placer', 'wld_ap'],
+    ['--placer', 'td_ap'],
+    ['--placer', 'wld_gp'],
+    ['--placer', 'td_gp', '--criticality_exponent', '4']
+]
+num_random_seeds = 8
 
 #####################
 # Place all ciruits #
 #####################
 os.chdir('..')
 subprocess.call(['./compile.sh'])
-place_caller = PlaceCaller(architecture, circuits_folder, circuits)
-place_caller.place_all(options, num_random_seeds)
-place_caller.save_results(results_file)
+
+for i in range(len(options)):
+    print('options: ' + ' '.join(options[i]))
+    place_caller = PlaceCaller(architecture, circuits_folder, circuits)
+    place_caller.place_all(options[i], num_random_seeds)
+    place_caller.save_results('place{0}.csv'.format(i))
