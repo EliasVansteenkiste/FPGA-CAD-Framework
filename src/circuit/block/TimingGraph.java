74f48a1f285845e61e1298ebf71acfba5585d937
Lebuin <seppe.lenders@gmail.com>
2016-01-05 10:59:34 +0100

    nest timinggraph classes

--- src/circuit/block/TimingGraph.java
+++ src/circuit/block/TimingGraph.java
@@ -18,7 +18,7 @@ import circuit.pin.AbstractPin;
 import placers.SAPlacer.Swap;
 import util.Triple;
 
-public class TimingGraph implements Iterable<TimingGraphEntry>, Serializable {
+public class TimingGraph implements Iterable<TimingGraph.TimingGraphEntry>, Serializable {
 
     private static final long serialVersionUID = 5056732903629079227L;
 
@@ -343,6 +343,8 @@ public class TimingGraph implements Iterable<TimingGraphEntry>, Serializable {
         // Do nothing
     }
 
+
+
     // Iterator methods
     // When iterating over a TimingGraph object, you will get a TimingGraphEntry
     // object for each connection in the timinggraph. Each of those objects contains
@@ -350,5 +352,104 @@ public class TimingGraph implements Iterable<TimingGraphEntry>, Serializable {
     @Override
     public Iterator<TimingGraphEntry> iterator() {
         return new TimingGraphIterator(this.circuit.getGlobalBlocks());
+    }
+
+    private class TimingGraphIterator implements Iterator<TimingGraphEntry> {
+
+        private GlobalBlock sourceGlobalBlock;
+        private Iterator<GlobalBlock> sourceGlobalBlockIterator;
+
+        private LeafBlock sourceBlock;
+        private Iterator<LeafBlock> sourceBlockIterator;
+
+        private int maxSinkIndex, sinkIndex;
+
+        private TimingGraphEntry cachedEntry = null;
+
+
+        TimingGraphIterator(List<GlobalBlock> globalBlocks) {
+            this.sourceGlobalBlockIterator = globalBlocks.iterator();
+
+            this.sourceGlobalBlock = this.sourceGlobalBlockIterator.next();
+            this.sourceBlockIterator = this.sourceGlobalBlock.getLeafBlocks().iterator();
+
+            this.sourceBlock = this.sourceBlockIterator.next();
+            this.maxSinkIndex = this.sourceBlock.getNumSinks();
+            this.sinkIndex = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if(this.cachedEntry != null) {
+                return true;
+            }
+
+            while(this.sinkIndex < this.maxSinkIndex) {
+
+                while(!this.sourceBlockIterator.hasNext()) {
+                    if(!this.sourceGlobalBlockIterator.hasNext()) {
+                        return false;
+                    }
+
+                    this.sourceGlobalBlock = this.sourceGlobalBlockIterator.next();
+                    this.sourceBlockIterator = this.sourceGlobalBlock.getLeafBlocks().iterator();
+                }
+
+                this.sourceBlock = this.sourceBlockIterator.next();
+                this.maxSinkIndex = this.sourceBlock.getNumSinks();
+                this.sinkIndex = 0;
+            }
+
+            LeafBlock sink = this.sourceBlock.getSink(this.sinkIndex);
+            TimingEdge edge = this.sourceBlock.getSinkEdge(this.sinkIndex);
+            this.sinkIndex++;
+
+            this.cachedEntry = new TimingGraphEntry(
+                    this.sourceGlobalBlock,
+                    sink.getGlobalParent(),
+                    edge.getCriticality(),
+                    this.sourceBlock.getNumSinks());
+
+            return true;
+        }
+
+        @Override
+        public TimingGraphEntry next() {
+            TimingGraphEntry entry = this.cachedEntry;
+            this.cachedEntry = null;
+            return entry;
+        }
+
+        @Override
+        public void remove() {
+            // Not supported
+        }
+    }
+
+    class TimingGraphEntry {
+        private GlobalBlock source;
+        private GlobalBlock sink;
+        private double criticality;
+        private int numSinks;
+
+        TimingGraphEntry(GlobalBlock source, GlobalBlock sink, double criticality, int numSinks) {
+            this.source = source;
+            this.sink = sink;
+            this.criticality = criticality;
+            this.numSinks = numSinks;
+        }
+
+        public GlobalBlock getSource() {
+            return this.source;
+        }
+        public GlobalBlock getSink() {
+            return this.sink;
+        }
+        public double getCriticality() {
+            return this.criticality;
+        }
+        public int getNumSinks() {
+            return this.numSinks;
+        }
     }
 }
