53e4ea2bc720f9b127d24d6d4a4865bf35d5c482
Dries Vercruyce <dries.vercruyce@ugent.be>
2017-01-05 17:08:21 +0100

    Changes to placement output file (.place)

--- src/place/circuit/io/PlaceDumper.java
+++ src/place/circuit/io/PlaceDumper.java
@@ -25,9 +25,12 @@ public class PlaceDumper {
         this.architectureFileVPR = architectureFileVPR;
 
 
-        String userDir = System.getProperty("user.dir");
-        this.netPath = this.netFile.getAbsolutePath().substring(userDir.length() + 1);
-        this.architecturePath = this.architectureFileVPR.getAbsolutePath().substring(userDir.length() + 1);
+        //String userDir = System.getProperty("user.dir");
+        //this.netPath = this.netFile.getAbsolutePath().substring(userDir.length() + 1);
+        //this.architecturePath = this.architectureFileVPR.getAbsolutePath().substring(userDir.length() + 1);
+        
+        this.netPath = this.netFile.getAbsolutePath();
+        this.architecturePath = this.architectureFileVPR.getAbsolutePath();
     }
 
 
@@ -43,15 +46,33 @@ public class PlaceDumper {
                 length = block.getName().length();
             }
         }
-
+        length = Math.max(length, 11);
 
         int width = this.circuit.getWidth(), height = this.circuit.getHeight();
 
         this.dumpHeader(writer, width, height, length);
 
+        int maxIndex = 0;
+        for(GlobalBlock block : this.circuit.getGlobalBlocks()){
+        	if(block.getIndex() > maxIndex){
+        		maxIndex = block.getIndex();
+        	}
+        }
+        GlobalBlock[] blocks = new GlobalBlock[maxIndex+1];
+        for(GlobalBlock block : this.circuit.getGlobalBlocks()){
+        	if(blocks[block.getIndex()] != null){
+        		System.out.println("Duplicate index: " + block.getIndex());
+        	}
+        	blocks[block.getIndex()] = block;
+        }
+        for(int index=0;index<blocks.length;index++){
+        	if(blocks[index] == null){
+        		System.out.println("Unused index: " + index);
+        	}
+        }
 
         Map<AbstractSite, Integer> siteOccupations = new HashMap<AbstractSite, Integer>();
-        for(GlobalBlock block : this.circuit.getGlobalBlocks()) {
+        for(GlobalBlock block : blocks) {
             AbstractSite site = block.getSite();
             int x = site.getColumn();
             int y = site.getRow();
@@ -65,12 +86,9 @@ public class PlaceDumper {
             }
             siteOccupations.put(site, z + 1);
 
-
-
-            writer.printf("%-"+length+"s %-7d %-7d %-7d #%d\n",
-                    block.getName(), x, y, z, index);
+            //writer.printf("%-"+length+"s %-7d %-7d %-7d #%d\n",block.getName(), x, y, z, index);
+            writer.printf("%-"+length+"s\t%d\t%d\t%d\t#%d\n",block.getName(), x, y, z, index);
         }
-
         writer.close();
     }
 
@@ -79,8 +97,7 @@ public class PlaceDumper {
         writer.printf("Netlist file: %s   Architecture file: %s\n", this.netPath, this.architecturePath);
         writer.printf("Array size: %d x %d logic blocks\n\n", width, height);
 
-        length = Math.max(length, 10);
-        writer.printf("%-"+length+"s x       y       subblk  block number\n", "#block name");
-        writer.printf("%-"+length+"s --      --      ------  ------------\n", "#----------");
+        writer.printf("%-"+length+"s\tx\ty\tsubblk\tblock number\n", "#block name");
+        writer.printf("%-"+length+"s\t--\t--\t------\t------------\n", "#----------");
     }
 }
