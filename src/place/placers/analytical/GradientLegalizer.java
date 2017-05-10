ddbcb5e84caa7d97323e61b169b3314e4c446d77
Dries Vercruyce <dries.vercruyce@ugent.be>
2017-05-10 17:56:55 +0200

    Detailed legalization after pushing spreader

--- src/place/placers/analytical/GradientLegalizer.java
+++ src/place/placers/analytical/GradientLegalizer.java
@@ -1,12 +1,10 @@
 package place.placers.analytical;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
 import place.circuit.Circuit;
-import place.circuit.architecture.BlockCategory;
 import place.circuit.architecture.BlockType;
 import place.circuit.block.GlobalBlock;
 import place.placers.analytical.AnalyticalAndGradientPlacer.Net;
@@ -15,19 +13,12 @@ import place.placers.analytical.AnalyticalAndGradientPlacer.TimingNet;
 import place.util.TimingTree;
 import place.visual.PlacementVisualizer;
 
-/*
- * In this legalizer we remove the illegal columns before spreading the blocks out.
- * This way we can spread the blocks on a homogeneous FPGA.
- * Afterwards the illegal columns are inserted.
- */
-
 class GradientLegalizer extends Legalizer {
 
 	private LegalizerBlock[] blocks;
 
-	final BlockType lab;
-    private int[] legalColumns;//1 if legal, 0 if illegal
     final PushingSpreader spreader;
+    private final DetailedLegalizer detailedLegalizer;
 
     private static final boolean timing = false;
 	private TimingTree timer = null;
@@ -52,28 +43,18 @@ class GradientLegalizer extends Legalizer {
             Map<GlobalBlock, NetBlock> netBlocks){
 
     	super(circuit, blockTypes, blockTypeIndexStarts, linearX, linearY, legalX, legalY, heights, nets, timingNets, visualizer, netBlocks);
-    	
-    	if(timing){
-    		this.timer = new TimingTree();
-    	}
 
-    	this.lab = BlockType.getBlockTypes(BlockCategory.CLB).get(0);
-    	this.legalColumns = new int[this.width + 2];
-    	for(int column = 0; column < this.width + 2; column++){
-    		if(this.circuit.getColumnType(column).equals(this.lab)){
-    			this.legalColumns[column] = 1;
-    		}else{
-    			this.legalColumns[column] = 0;
-    		}
-    	}
+    	this.timer = new TimingTree(timing);
     	
     	this.spreader = new PushingSpreader(
-    			Arrays.stream(this.legalColumns).sum(),	//width
-    			this.height,							//height
-    			1.00,									//step size
-    			0.2,									//speed averaging
+    			this.width,			//width
+    			this.height,		//height
+    			0.4,				//step size
+    			0.2,				//speed averaging
     			this.timer);
 
+    	this.detailedLegalizer = new DetailedLegalizer(circuit);
+    	
     	if(visual){
     		this.visualX = new double[this.linearX.length];
     		this.visualY = new double[this.linearY.length];
@@ -84,20 +65,14 @@ class GradientLegalizer extends Legalizer {
         if(timing) this.timer.start("Legalize BlockType");
 
         this.initializeData(blocksStart, blocksEnd);
-        this.removeIllegalColumns();
-
-        this.addVisual("Remove illegal columns");
         	
-        //Initial Spreading
         this.spreader.doSpreading(this.blocks, this.blocks.length);
         this.addVisual("Initial spreading");
         
-        this.updateLegal();
-        this.addVisual("Update Legal");
-       	
-       	this.insertIllegalColumns();
+        this.detailedLegalizer.legalize(this.blocks);
+        this.addVisual("Detailed legalizer");
+
        	this.updateLegal();
-       	this.addVisual("Insert illegal columns");
         
        	if(timing) this.timer.time("Legalize BlockType");
     }
@@ -128,111 +103,7 @@ class GradientLegalizer extends Legalizer {
     	
     	if(timing) this.timer.time("Initialize Data");
     }
-    void removeIllegalColumns(){
-    	List<ScaleCluster> clusters = new ArrayList<ScaleCluster>();
-    	
-    	int counter = 0;
-    	while(this.legalColumns[counter] == 0) counter++;
-    	this.findScaleClusters(0.0, counter, clusters);
-    	
-    	//for(ScaleCluster cluster:clusters){
-    	//	System.out.println(cluster);
-    	//}
-    	
-    	for(ScaleCluster cluster:clusters){
-    		for(LegalizerBlock block:this.blocks){
-    			if(block.horizontal.coordinate >= cluster.illegalStart - 0.5){
-    				if(block.horizontal.coordinate < cluster.illegalEnd - 0.5){
-    					cluster.add(block);
-    				}
-    			}
-    		}
-    	}
-    	for(ScaleCluster cluster:clusters){
-    		cluster.scale();
-    	}
-    	
-    	this.addVisual("before subtract");
-    	
-    	double subtract = 0.0;
-    	for(ScaleCluster cluster:clusters){
-    		subtract += cluster.legalStart - cluster.illegalStart;
-    		
-    		for(LegalizerBlock block:cluster.blocks){
-    			block.horizontal.coordinate -= subtract;
-    			
-    			if(block.horizontal.coordinate < 0){
-    				block.horizontal.coordinate = 0.1;
-    			}
-    		}
-    		
-    		subtract += cluster.illegalEnd - cluster.legalEnd;
-    	}
-    }
-    void findScaleClusters(double illegalStart, int counter, List<ScaleCluster> clusters){
-    	
-    	int legalStart = counter;
-    	while(this.legalColumns[counter] == 1) counter++;
-    	int legalEnd = counter;
-
-    	int illegalColumns = 0;
-        while(this.legalColumns[counter] == 0){
-        	illegalColumns++;
-        	counter++;
-
-        	if(counter == this.width + 2){
-           		double illegalEnd = counter;
-           		clusters.add(new ScaleCluster(illegalStart, legalStart, illegalEnd, legalEnd));
-           		return;
-           	}
-        }
-        
-       	double illegalEnd = (double)counter - illegalColumns / 2.0;
-       	clusters.add(new ScaleCluster(illegalStart, legalStart, illegalEnd, legalEnd));
-       	this.findScaleClusters(illegalEnd, counter, clusters);
-    }
-    class ScaleCluster {
-    	double illegalStart;
-    	int legalStart;
-    	
-    	double illegalEnd;
-    	int legalEnd;
-    	
-    	ArrayList<LegalizerBlock> blocks;
-    	
-    	ScaleCluster(double illegalStart, int legalStart, double illegalEnd, int legalEnd){
-    		this.illegalStart = illegalStart;
-    		this.legalStart = legalStart;
-    		this.illegalEnd = illegalEnd;
-    		this.legalEnd = legalEnd;
-    		
-    		this.blocks = new ArrayList<LegalizerBlock>();
-    	}
-    	void add(LegalizerBlock block){
-    		this.blocks.add(block);
-    	}
-    	void scale(){
-    		double illegalLeft = this.legalStart - this.illegalStart;
-    		double illegalRight = this.illegalEnd - this.legalEnd;
-    		
-    		double illegalWidth = this.illegalEnd - this.illegalStart;
-    		
-    		double center = this.illegalStart - 0.5 + illegalWidth * illegalLeft / (illegalLeft + illegalRight);
-    		
-    		double scalingFactor = (center - this.legalStart + 0.5) / (center - this.illegalStart + 0.5);
-    		
-    		for(LegalizerBlock block:this.blocks){
-    			block.horizontal.coordinate = center - (center - block.horizontal.coordinate) * scalingFactor;
-    		}
-    	}
-    	
-    	public String toString(){
-    		String s = new String();
-    		s += "Cluster | " + String.format("%.1f", this.illegalStart) + " | " + String.format("%3d", this.legalStart) + " | --- | " + String.format("%3d", this.legalEnd) + " | " + String.format("%.1f", this.illegalEnd);
-    		return s;
-    	}
-    }
-
+    
     //Set legal coordinates of all blocks
     private void updateLegal(){
     	if(timing) this.timer.start("Update Legal");
@@ -249,27 +120,6 @@ class GradientLegalizer extends Legalizer {
     	}
     	
     	if(timing) this.timer.time("Update Legal");
-    }
-    private void insertIllegalColumns(){
-    	int[] newColumn = new int[Arrays.stream(this.legalColumns).sum() + 2];
-    	for(int i = 0; i < newColumn.length; i++){
-    		newColumn[i] = 0;
-    	}
-    	int illegalIncrease = 0;
-
-    	newColumn[0] = 0;
-    	for(int column = 1; column < this.width + 1; column++){
-    		if(this.circuit.getColumnType(column).equals(this.blockType)){
-    			newColumn[column - illegalIncrease] = column;
-    		}else{
-    			illegalIncrease += 1;
-    		}
-    	}
-    	newColumn[this.width + 1 - illegalIncrease] = this.width + 1;
-    	
-    	for(LegalizerBlock block:this.blocks){
-    		block.horizontal.coordinate = newColumn[(int)Math.round(block.horizontal.coordinate)];
-    	}
     }
     
     // Visual
@@ -298,6 +148,8 @@ class GradientLegalizer extends Legalizer {
     	final int height;
     	
     	double a1, a2, a3, a4;
+
+    	boolean processed;
     	
     	LegalizerBlock(int index, double x, double y, int offset, int height){
     		this.index = index;
@@ -320,8 +172,11 @@ class GradientLegalizer extends Legalizer {
     		this.a3 = xRight * yLeft;
     		this.a4 = xRight * yRight;
     	}
+    	
+    	double cost(int legalX, int legalY){
+    		return (this.horizontal.coordinate - legalX) * (this.horizontal.coordinate - legalX) + (this.vertical.coordinate - legalY) * (this.vertical.coordinate - legalY);
+    	}
     }
-    
     class Dimension {
     	double coordinate;
     	double speed;
@@ -345,5 +200,13 @@ class GradientLegalizer extends Legalizer {
             	this.coordinate += this.speed;
     		}
     	}
+    }
+    public static class Comparators {
+    	public static Comparator<LegalizerBlock> VERTICAL = new Comparator<LegalizerBlock>() {
+    		@Override
+    		public int compare(LegalizerBlock b1, LegalizerBlock b2) {
+    			return Double.compare(b1.vertical.coordinate, b2.vertical.coordinate);
+    		}
+    	};
     }
 }
