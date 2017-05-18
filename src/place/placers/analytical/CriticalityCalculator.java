dd90be8fde5ff56d2ffc98fc100812f021c219ac
Dries Vercruyce <dries.vercruyce@ugent.be>
2017-05-18 11:37:54 +0200

    Use exact delay in criticality calculator

--- src/place/placers/analytical/CriticalityCalculator.java
+++ src/place/placers/analytical/CriticalityCalculator.java
@@ -1,6 +1,6 @@
 package place.placers.analytical;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -14,17 +14,9 @@ import place.placers.analytical.AnalyticalAndGradientPlacer.TimingNet;
 import place.placers.analytical.AnalyticalAndGradientPlacer.TimingNetBlock;
 
 class CriticalityCalculator{
-
-    private int width, height;
-
     private TimingGraph timingGraph;
     private DelayTables delayTables;
     private BlockCategory[] blockCategories;
-    
-    private double iotoio;
-    private double iotoclb;
-    private double clbtoio;
-    private double clbtoclb;
 
     private List<TimingNet> nets;
 
@@ -32,9 +24,6 @@ class CriticalityCalculator{
             Circuit circuit,
             Map<GlobalBlock, NetBlock> netBlocks,
             List<TimingNet> nets) {
-
-        this.width = circuit.getWidth();
-        this.height = circuit.getHeight();
 
         this.timingGraph = circuit.getTimingGraph();
         this.delayTables = circuit.getArchitecture().getDelayTables();
@@ -48,58 +37,6 @@ class CriticalityCalculator{
         }
 
         this.nets = nets;
-        
-        this.initializeAvarageDelay();
-    }
-    private void initializeAvarageDelay(){
-        BlockCategory clb = BlockCategory.CLB;
-        BlockCategory io = BlockCategory.IO;
-        this.clbtoclb = calculateAverageDelay(clb,clb);
-        this.clbtoio = calculateAverageDelay(clb, io);
-        this.iotoclb = calculateAverageDelay(io, clb);
-        this.iotoio = calculateAverageDelay(io, io);
-
-        System.out.printf("Average delay\n");
-        System.out.printf("\tCLB -> CLB\t%2.2e\n", this.clbtoclb);
-        System.out.printf("\tCLB -> IO\t%2.2e\n", this.clbtoio);
-        System.out.printf("\tIO -> CLB\t%2.2e\n", this.iotoclb);
-        System.out.printf("\tIO -> IO\t%2.2e\n\n", this.iotoio);
-
-    }
-    private double calculateAverageDelay(BlockCategory sourceCategory, BlockCategory sinkCategory){
-        List<Double> elements = new ArrayList<>();
-
-        int width = this.width;
-        int height = this.height;
-
-        if(sourceCategory.equals(BlockCategory.IO)){
-        	width += 1;
-        	height += 1;
-        }
-        if(sinkCategory.equals(BlockCategory.IO)){
-        	width += 1;
-        	height += 1;
-        }
-
-        for(int deltaX = 0; deltaX < width; deltaX++){
-        	for(int deltaY = 0; deltaY < height; deltaY++){
-        		if(deltaX + deltaY > 0){
-        			double delay = this.delayTables.getDelay(sourceCategory, sinkCategory, deltaX, deltaY);
-        			if(delay > 0){
-        				double unitDelay = delay / (deltaX + deltaY);
-        				elements.add(unitDelay);
-        			}
-        		}
-        	}
-        }
-        return average(elements);
-    }
-    private double average(List<Double> list){
-    	double sum = 0.0;
-    	for(double element:list){
-    		sum += element;
-    	}
-    	return sum / list.size();
     }
 
     public double calculate(int[] x, int[] y) {
@@ -127,25 +64,9 @@ class CriticalityCalculator{
                 int deltaX = Math.abs(sinkX - sourceX);
                 int deltaY = Math.abs(sinkY - sourceY);
 
-                double wireDelay = (deltaX  + deltaY) * this.getUnitDelay(sourceCategory, sinkCategory);
-                //double wireDelay = this.delayTables.getDelay(sourceCategory, sinkCategory, deltaX, deltaY);
+                double wireDelay = this.delayTables.getDelay(sourceCategory, sinkCategory, deltaX, deltaY);
                 sink.timingEdge.setWireDelay(wireDelay);
             }
         }
-    }
-    private double getUnitDelay(BlockCategory sourceCategory, BlockCategory sinkCategory){
-    	if(sourceCategory.equals(BlockCategory.IO)){
-    		if(sinkCategory.equals(BlockCategory.IO)){
-    			return this.iotoio;
-    		}else{
-    			return this.iotoclb;
-    		}
-    	}else{
-    		if(sinkCategory.equals(BlockCategory.IO)){
-    			return this.clbtoio;
-    		}else{
-    			return this.clbtoclb;
-    		}
-    	}
     }
 }
