7f924146fa3bd4269ed07d02743367e902722c31
Dries Vercruyce <dries.vercruyce@ugent.be>
2017-06-16 19:37:16 +0200

    New scheme for adaptive learning rate

--- src/place/placers/analytical/GradientPlacerTD.java
+++ src/place/placers/analytical/GradientPlacerTD.java
@@ -95,7 +95,7 @@ public class GradientPlacerTD extends GradientPlacer {
 
         if(iteration > 0) {
             this.anchorWeight = Math.pow((double)iteration / (this.numIterations - 1.0), this.anchorWeightExponent) * this.anchorWeightStop;
-            this.learningRate *= this.learningRateMultiplier;
+            this.learningRate = this.learningRateStart - Math.pow((double)iteration / (this.numIterations - 1.0), this.learningRateExponent) * (this.learningRateStart - this.learningRateStop);
             this.quality *= this.qualityMultiplier;
         }
     }
