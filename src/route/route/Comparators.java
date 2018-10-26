4ad4ddb8eb5de3a10eec8254222c61f82f3ba1bd
Dries Vercruyce <dries.vercruyce@ugent.be>
2018-10-26 11:21:58 +0200

    Refactoring of comparators and criticality comparator

--- src/route/route/Comparators.java
+++ src/route/route/Comparators.java
@@ -21,4 +21,86 @@ public class Comparators {
             }
         }
     };
+    
+    public static Comparator<Connection>  BBComparator = new Comparator<Connection>() {
+    	@Override
+    	public int compare(Connection a, Connection b) {
+    		if(a.boundingBox > b.boundingBox){
+    			return 1;
+    		}else if(a.boundingBox == b.boundingBox){
+    			if(a.net.fanout > b.net.fanout){
+    				return 1;
+    			}else if(a.net.fanout == b.net.fanout){
+    				if(a.hashCode() > b.hashCode()){
+    					return 1;
+    				}else if(a.hashCode() < b.hashCode()){
+    					return -1;
+    				}else{
+    					if(a != b) System.out.println("Failure: Error while comparing 2 connections. HashCode of Two Connections was identical");
+    					return 0;
+    				}
+    			}else{
+    				return -1;
+    			}
+    		}else{
+    			return -1;
+    		}
+    	}
+    };
+    public static Comparator<Connection>  FanoutComparator = new Comparator<Connection>() {
+    	@Override
+    	public int compare(Connection a, Connection b) {
+    		if(a.net.fanout < b.net.fanout){
+    			return 1;
+    		}else if(a.net.fanout == b.net.fanout){
+    			if(a.boundingBox > b.boundingBox){
+    				return 1;
+    			}else if(a.boundingBox == b.boundingBox){
+    				if(a.hashCode() > b.hashCode()){
+    					return 1;
+    				}else if(a.hashCode() < b.hashCode()){
+    					return -1;
+    				}else{
+    					if(a != b) System.out.println("Failure: Error while comparing 2 connections. HashCode of Two Connections was identical");
+    					return 0;
+    				}
+    			}else{
+    				return -1;
+    			}
+    		}else{
+    			return -1;
+    		}
+    	}
+    };
+    public static Comparator<Connection>  CriticalityComparator = new Comparator<Connection>() {
+    	@Override
+    	public int compare(Connection a, Connection b) {
+    		if(a.getCriticality() < b.getCriticality()){
+    			return 1;
+    		}else if(a.getCriticality() == b.getCriticality()){
+    			if(a.net.fanout < b.net.fanout){
+    				return 1;
+    			}else if(a.net.fanout == b.net.fanout){
+    				if(a.boundingBox < b.boundingBox){
+    					return 1;
+    				}else if(a.boundingBox == b.boundingBox){
+    					if(a.hashCode() > b.hashCode()){
+        					return 1;
+        				}else if(a.hashCode() < b.hashCode()){
+        					return -1;
+        				}else{
+        					if(a != b) System.out.println("Failure: Error while comparing 2 connections. HashCode of Two Connections was identical");
+        					return 0;
+        				}
+    				}else {
+    					return -1;
+    				}
+    			}else{
+    				return -1;
+    			}
+    		}else{
+    			return -1;
+    		}
+    	}
+    };
 }
